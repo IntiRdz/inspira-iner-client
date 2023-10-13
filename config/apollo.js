9d9cda9ed28e5fab397d5f660726fdb917db4710
IntiRdz <72955312+IntiRdz@users.noreply.github.com>
2023-10-12 21:13:40 -0600

    UVEHv1.1

--- config/apollo.js
+++ config/apollo.js
@@ -3,7 +3,7 @@ import { createHttpLink } from '@apollo/client/link/http';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000',
+  uri: 'https://inspira-iner-server-a98ddf825333.herokuapp.com/',
 });
 
 const authLink = setContext((_, { headers }) => {
