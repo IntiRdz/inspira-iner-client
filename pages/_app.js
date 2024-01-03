480d2032f402ee2c721d749fa8e173f40efa32ab
Inti Rodríguez <neumo.inti.rdz@gmail.com>
2024-01-03 16:44:34 -0600

    Apollo1

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,18 @@
-import { ApolloProvider } from '@apollo/client';
-import { useEffect, useState } from 'react';
 import '../styles/globals.css';
+import { ApolloProvider } from '@apollo/client';
+import client from '../apollo-client';
+
+
 import PacienteState from '../context/pacientes/PacienteState';
 import { AuthProvider } from '../context/usuarios/AuthContext';
 
 
 
 function MyApp({ Component, pageProps }) {
-  const [apolloClient, setApolloClient] = useState(null);
 
-  useEffect(() => {
-    import('../config/apollo').then((module) => {
-      setApolloClient(module.default);
-    });
-  }, []);
-
-  if (!apolloClient) {
-    return <div>Loading...</div>;
-  }
 
   return (
-    <ApolloProvider client={apolloClient}>
+    <ApolloProvider client={client}>
       <AuthProvider>
           <PacienteState>
             <Component {...pageProps} />
