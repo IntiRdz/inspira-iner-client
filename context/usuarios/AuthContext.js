3acffcb3fdedf8d72a2b7aa5999b95367f98ced6
IntiRdz <72955312+IntiRdz@users.noreply.github.com>
2024-01-04 16:27:06 -0600

    Update AuthContext.js

--- context/usuarios/AuthContext.js
+++ context/usuarios/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, useCallback } from 'react';
+import React, { createContext, useState, useEffect, useCallback, useMemo } from 'react';
 import { useRouter } from 'next/router';
 import jwtDecode from 'jwt-decode';
 
